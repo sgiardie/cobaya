48dd885cebbaa97aea705513c62126842af3193e
sgiardie <grdsrn1@unife.it>
2022-03-11 18:20:41 +0100

    combined likelihood for class and spider data

--- v_like/v_like.py
+++ v_like/v_like.py
@@ -8,39 +8,29 @@ class VLike(Likelihood):
         Importing data file cl_file set in .yaml file
         cl_file can be Vmodes_CLASS.txt or spider(150/95).txt
         """
-        self.data = np.loadtxt(self.cl_file, skiprows = 1)
-        
+        self.data_C = np.loadtxt(self.cl_class, skiprows = 1)
+        self.data_sp150 = np.loadtxt(self.cl_sp150, skiprows = 1)
+        self.data_sp95 = np.loadtxt(self.cl_sp95, skiprows = 1)
+
         #binning for CLASS data: bin center from file
         #each bin is 12 ell large
-        if self.data.shape[1] == 3:
-            #self.bin_center = self.data[:,0]
-            self.bin_min = self.data[:,0].astype('int')-6*np.ones(self.data[:,0].size,dtype = int)
-            self.bin_max = self.data[:,0].astype('int')+5*np.ones(self.data[:,0].size,dtype = int)
-            self.ClVV_data = self.data[:,1]
-            self.ClVV_err = self.data[:,2]
-        
-        #other class data file:
-        if self.data.shape[1] == 4:
-            self.bin_min = self.data[:,0].astype('int')-5*np.ones(self.data[:,0].size,dtype = int)
-            self.bin_max = self.data[:,0].astype('int')+6*np.ones(self.data[:,0].size,dtype = int)
-            self.ClVV_data = self.data[:,1]
-            self.ClVV_err = self.data[:,2]
+        self.lmin_C =  np.arange(10)*12+1
+        self.lmax_C = (np.arange(10)+1)*12
+        self.ClVV_data_C = self.data_C[:,1]
+        self.ClVV_err_C = self.data_C[:,2]
 
 
         #spider data files
-        elif self.data.shape[1] == 6:
-            self.bin_min = self.data[:,0]
-            #self.bin_center = self.data[:,1]
-            self.bin_max = self.data[:,2]
-            self.ClVV_data = self.data[:,3]
-            #self.ClVV_perr = self.data[:,4]
-            #self.ClVV_nerr = self.data[:,5]
-            self.ClVV_err = (self.data[:,4]-self.data[:,5])/2
+        self.lmin_sp150 = self.data_sp150[:,0].astype('int')
+        self.lmax_sp150 = self.data_sp150[:,2].astype('int')
+        self.ClVV_data_sp150 = self.data_sp150[:,3]
+        self.ClVV_err_sp150 = (self.data_sp150[:,4]-self.data_sp150[:,5])/2
 
-        else:
-            print("Error: not using the correct spider or Class data!")
+        self.lmin_sp95 = self.data_sp95[:,0].astype('int')
+        self.lmax_sp95 = self.data_sp95[:,2].astype('int')
+        self.ClVV_data_sp95 = self.data_sp95[:,3]
+        self.ClVV_err_sp95 = (self.data_sp95[:,4]-self.data_sp95[:,5])/2
 
-       
         self.log.info("Initialized!")
 
     def get_requirements(self):
@@ -49,14 +39,14 @@ class VLike(Likelihood):
 
          e.g. here we need C_L^{vv} to lmax=2500 and the H0 value
         """
-        return {'Cl': {'vv': self.bin_max[-1]}} 
+        return {'Cl': {'vv': self.lmax_sp150[-1]}} 
 
     def logp(self, **params_values):
         """
         Taking a dictionary of (sampled) nuisance parameter values params_values
         and return a log-likelihood.
         """
-        ClVV_th = self.provider.get_Cl(ell_factor = False)['vv']
+        ClVV_th = self.provider.get_Cl(ell_factor = True)['vv']
         return self.log_likelihood(ClVV_th, **params_values)
 
     def log_likelihood(self, ClVV_th, **params_values):
@@ -64,10 +54,18 @@ class VLike(Likelihood):
         Actual calculation of the log likelihood
         """
         chi2 =  0
-        for i in range(self.data.shape[0]):
-            ClVV_th_bin = ClVV_th[self.bin_min[i]:self.bin_max[i]].sum()/(self.bin_max[i]-self.bin_min[i]+1)
-            chi2 += (ClVV_th_bin - self.ClVV_data[i])**2/(self.ClVV_err[i])**2
-        
+        for i in range(self.data_C.shape[0]):
+            ClVV_th_bin_C = ClVV_th[self.lmin_C[i]:self.lmax_C[i]].sum()/(self.lmax_C[i]-self.lmin_C[i]+1)
+            chi2 += (ClVV_th_bin_C - self.ClVV_data_C[i])**2/(self.ClVV_err_C[i])**2
+       
+        for i in range(self.data_sp150.shape[0]):
+            ClVV_th_bin_sp150 = ClVV_th[self.lmin_sp150[i]:self.lmax_sp150[i]].sum()/(self.lmax_sp150[i]-self.lmin_sp150[i]+1)
+            chi2 += (ClVV_th_bin_sp150 - self.ClVV_data_sp150[i])**2/(self.ClVV_err_sp150[i])**2
+          
+        for i in range(self.data_sp95.shape[0]):
+            ClVV_th_bin_sp95 = ClVV_th[self.lmin_sp95[i]:self.lmax_sp95[i]].sum()/(self.lmax_sp95[i]-self.lmin_sp95[i]+1)
+            chi2 += (ClVV_th_bin_sp95 - self.ClVV_data_sp95[i])**2/(self.ClVV_err_sp95[i])**2
+
         return -chi2/2.
 
 
